b76ee4e75ac8bbc9b781bd6e0b8b51081c6641a5
Carlos Santos <carlos.santosx85@hotmail.com>
2023-03-21 16:33:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Page_cliente = st.sidebar.selectbox('Cliente', ['Incluir', 'Consultar'], 0)
 
 if Page_cliente == 'Consultar': 
 
-if Page_cliente == 'Incluir':
+    if Page_cliente == 'Incluir':
     
 
     
