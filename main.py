cc8f2c0fcc417bc6933bd05c43a733b1a62953de
Carlos Santos <carlos.santosx85@hotmail.com>
2023-03-21 16:32:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 Page_cliente = st.sidebar.selectbox('Cliente', ['Incluir', 'Consultar'], 0)
 
-    if Page_cliente == 'Consultar': 
+if Page_cliente == 'Consultar': 
 
-    if Page_cliente == 'Incluir':
+if Page_cliente == 'Incluir':
     
 
     
