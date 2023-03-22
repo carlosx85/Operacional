a128a4291188fe6b3b84eae0acc432301772ef94
Carlos Santos <carlos.santosx85@hotmail.com>
2023-03-22 14:58:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import streamlit as st
+import hydralit_components as hc
 
-Page_cliente = st.sidebar.selectbox(
-    'Cliente', ['Incluir', 'Consultar'], 0)
+# specify the primary menu definition
+menu_data = [
+        {'icon': "far fa-copy", 'label':"Left End"},
+        {'id':'Copy','icon':"🐙",'label':"Copy"},
+        {'icon': "far fa-chart-bar", 'label':"Chart"},#no tooltip message
+        {'icon': "far fa-address-book", 'label':"Book"},
+        {'id':' Crazy return value 💀','icon': "💀", 'label':"Calendar"},
+        {'icon': "far fa-clone", 'label':"Component"},
+        {'icon': "fas fa-tachometer-alt", 'label':"Dashboard",'ttip':"I'm the Dashboard tooltip!"}, #can add a tooltip message
+        {'icon': "far fa-copy", 'label':"Right End"},
+]
+# we can override any part of the primary colors of the menu
+#over_theme = {'txc_inactive': '#FFFFFF','menu_background':'red','txc_active':'yellow','option_active':'blue'}
+over_theme = {'txc_inactive': '#FFFFFF'}
+menu_id = hc.nav_bar(menu_definition=menu_data,home_name='Home',override_theme=over_theme)
 
-if Page_cliente == 'Incluir':
-    st.experimental_set_query_params()
-    st.write("""Incluir""")
-
-if Page_cliente == 'Consultar':
-    st.experimental_set_query_params()
-    st.write("""Consultar""")
-
+    
+#get the id of the menu item clicked
+st.info(f"{menu_id=}")
 
     
